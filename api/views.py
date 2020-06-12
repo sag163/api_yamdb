3642d3379500d764e3443f59ee85a4cee917bf06
frolkit <60374189+frolkit@users.noreply.github.com>
2020-06-13 01:05:03 +0300

    Add filters in category genre titles (#6)
    * add django_filters
    
    * add filters in category, genre, titles

--- api/views.py
+++ api/views.py
@@ -1,5 +1,6 @@
-from rest_framework import viewsets, mixins
+from rest_framework import viewsets, mixins, filters
 from django.shortcuts import render
+from django_filters.rest_framework import DjangoFilterBackend
 
 from .models import Category, Genre, Title
 from .serializers import (CategorySerializer,
@@ -12,8 +13,9 @@ class CategoryViewSet(mixins.CreateModelMixin,
                       viewsets.GenericViewSet):
     queryset = Category.objects.all()
     serializer_class = CategorySerializer
+    filter_backends = [DjangoFilterBackend]
+    filterset_fields = ['slug', ]
     lookup_field = 'slug'
-
 
 class GenreViewSet(mixins.CreateModelMixin,
                    mixins.ListModelMixin,
@@ -21,9 +23,15 @@ class GenreViewSet(mixins.CreateModelMixin,
                    viewsets.GenericViewSet):
     queryset = Genre.objects.all()
     serializer_class = GenreSerializer
+    filter_backends = [DjangoFilterBackend]
+    filterset_fields = ['slug', ] 
     lookup_field = 'slug'
 
 
 class TitleViewSet(viewsets.ModelViewSet):
     queryset = Title.objects.all()
     serializer_class = TitleSerializer
+    filter_backends = [DjangoFilterBackend, filters.SearchFilter]
+    filterset_fields = ['category', 'genre', 'year', ]
+    search_fields = ['name', ]
+    
