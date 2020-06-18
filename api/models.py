de07fadb8f4b795afd719769c2f34864a5307eb7
Nikita <frolkit@gmail.com>
2020-06-18 20:32:33 +0300

    update code for tests

--- api/models.py
+++ api/models.py
@@ -8,24 +8,28 @@ User = get_user_model()
 class Category(models.Model):
     name = models.TextField()
     slug = models.SlugField(unique=True)
-
+    
+    def __str__(self):
+        return self.name
 
 class Genre(models.Model):
     name = models.TextField()
     slug = models.SlugField(unique=True)
 
+    def __str__(self):
+        return self.name
+
 
 class Title(models.Model):
     name = models.TextField()
     year = models.IntegerField()
+    rating = models.IntegerField(blank=True, null=True)
+    description = models.TextField()
     category = models.ForeignKey(Category,
                                  on_delete=models.SET_NULL,
                                  blank=True,
                                  null=True)
-    genre = models.ForeignKey(Genre,
-                              on_delete=models.SET_NULL,
-                              blank=True,
-                              null=True)
+    genre = models.ManyToManyField(Genre)
 
 
 class Review(models.Model):
--- api/serializers.py
+++ api/serializers.py
@@ -4,33 +4,45 @@ from rest_framework.exceptions import ValidationError
 from .models import Category, Genre, Title, Review, Comment
 
 
+class CustomSlugRelatedField(serializers.SlugRelatedField):
+    def to_representation(self, obj):
+        # return {'name': obj.name, 'slug': obj.slug} # страница выдаёт ошибку unhashable type: 'dict'
+        return f"'name': {obj.name}, 'slug': {obj.slug}" # эта строчка не проходит тесты из-за ковычек
+
+
 class CategorySerializer(serializers.ModelSerializer):
     class Meta:
-        fields = ('id', 'name', 'slug')
+        fields = ('name', 'slug')
         model = Category
 
 
 class GenreSerializer(serializers.ModelSerializer):
-     class Meta:
-        fields = ('id', 'name', 'slug')
+    class Meta:
+        fields = ('name', 'slug')
         model = Genre
 
 
 class TitleSerializer(serializers.ModelSerializer):
-     class Meta:
-        fields = ('id', 'name', 'year', 'genre', 'category')
-        model = Title
+    rating = serializers.ReadOnlyField()
+    category = CustomSlugRelatedField(queryset=Category.objects.all(), 
+                                     slug_field='slug')
+    genre = CustomSlugRelatedField(queryset=Genre.objects.all(),
+                                   slug_field='slug',
+                                   many=True)
+
+    class Meta:
+        fields = ('id', 'name', 'year', 'rating', 'description', 'genre', 'category')
+        model = Title    
 
 
 class ReviewSerializer(serializers.ModelSerializer):
-        author = serializers.ReadOnlyField(source='author.username')
-        class Meta:
-                fields = ('id', 'title', 'text', 'author', 'score', 'pub_date')
-                model = Review
+    author = serializers.ReadOnlyField(source='author.username')
+    class Meta:
+        fields = ('id', 'title', 'text', 'author', 'score', 'pub_date')
+        model = Review
 
 
 class CommentSerializer(serializers.ModelSerializer):
-       class Meta:
+    class Meta:
         fields = ('id', 'review', 'text', 'author', 'score', 'pub_date')
         model = Comment
-
