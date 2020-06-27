3b35576b786ea6c129df81cc3cb704a790b5247d
Nikolai586 <60741925+Nikolai586@users.noreply.github.com>
2020-06-27 18:48:18 +0300

    Update permission.py

--- api/permission.py
+++ api/permission.py
@@ -1,5 +1,12 @@
 from rest_framework import permissions
 
 class IsModeratorPermission(permissions.BasePermission):
+    
     def has_permission(self, request, view):
-        return request.user.role == 'moderator'
+        if request.user.is_authenticated:
+            return request.user.role == 'moderator'
+        
+class IsAuthorPermission(permissions.BasePermission):
+
+    def has_object_permission(self, request, view, obj):
+        return request.user == obj.author
